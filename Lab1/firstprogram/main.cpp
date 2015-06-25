0c26dbe4454f3a4d40a93aff955fe1c17351c4c0
joseaguirre <j.r.aguirre@live.com>
2015-06-25 10:23:08 -0700

    Signed-off-by: joseaguirre <j.r.aguirre@live.com>

---
+++ Lab1/firstprogram/main.cpp
@@ -0,0 +1,74 @@
+//1st Program
+
+/*
+
+ *File:	main.cpp
+
+ *Author: Dr. Mark E. Lehr
+
+ *Transcribed: Jose Aguirre
+
+ *Created on June 22, 2015, 12:35 PM
+
+ *Purpose: First Program to test the IDE
+
+ */
+
+
+
+
+//System Libraries
+
+
+
+
+#include <iostream> //File I/O
+
+using namespace std; //std namespace -> iostream
+
+
+
+
+//User Libraries
+
+
+
+
+//Global Constants
+
+
+
+
+//Function Prototypes
+
+
+
+
+//Execution Begins Here!
+
+int main(int argc, char** argv) {
+
+	//Declare Variables Here
+
+
+
+
+	//Input Values Here
+
+
+
+
+	//Process Input Here
+
+
+
+
+	//Output Unknowns Here
+
+	cout<<"Hello World"<<endl;
+
+	//Exit Stage Right!
+
+	return 0;
+
+}
